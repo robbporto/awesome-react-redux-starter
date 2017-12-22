1507f22811bb84ccf8d2636ee76019ca1ac3cba7
Robson Porto <robbporto@gmail.com>
2017-12-22 15:33:48 -0300

    Adding .eslintignore and fixing some files.

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
-import home from './modules/Home/redux/Home.reducers'
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import home from './modules/Home/redux/Home.reducers';
 
 export default combineReducers({
   routing: routerReducer,
-  home
-})
+  home,
+});
